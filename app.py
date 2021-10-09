3e0305bfcd06c29cd55e8ddf6f7bfc47026b6a44
jonathan canola salazar jorge canola salazar <desasoftware23@hotmail.com>
2021-10-08 22:21:51 -0500

    comienzo sprint 3 grupo 7 IBMW

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/login')
+@app.route('/login/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home',methods=["GET","POST"])
+def home():
+    return render_template('contents/home.html')
+
+@app.route('/usuarios',methods=["GET"])
+def usuarios():
+    return render_template('contents/usuarios.html')
+
+@app.route('/productos')
+def productos():
+    return render_template('contents/productos.html')
+
+@app.route('/proveedores')
+def proveedores():
+    return render_template('contents/proveedores.html')
+
+@app.route('/jonathan')
+def jonathan():
+    return render_template('contents/jonathan.html')
+
+@app.route('/producto')
+def producto():
+    return render_template('contents/producto.html')
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
---
+++ static/js/myJs.js
@@ -0,0 +1,9 @@
+
+// let btns = document.querySelectorAll(".buttons");
+// btns.forEach(li => {
+//   li.addEventListener('click', function() {
+//     btns.forEach(btn => btn.classList.remove('active'));
+//       this.classList.add('active');
+//   });
+// });
+
